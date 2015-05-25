325e9e32f8e69fa45876754db018408ffdcf0d47
Bianca Binstock <bianca.pr@gmail.com>
2015-05-25 10:34:32 -0400

    Initial Commit

---
+++ TinderMechanic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderMechanic
+//
+//  Created by Bianca Binstock on 2015-05-25.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
